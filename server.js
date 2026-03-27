b8f3caabba4b26e86e6d8e8c70a8608bb9868839
Sarath <sarathkumarsm16@gmail.com>
2026-03-27 16:01:59 +0530

    fix socket url

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
